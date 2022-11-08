fa6bcb0234e54a6e46e52bb05133fd40bc064592
Chris <CK.AH@outlook.com>
2022-11-08 21:43:03 +0000

    Add project files.

---
+++ ScreamingConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = "";
+Console.WriteLine("Shall I scream on your behalf? (Y/N)");
+input = Console.ReadLine();
+
+if (input == "Y" || input == "y")
+    {
+    Console.WriteLine("AAAAAAAAAAAAAaaaAAAaaAAAAaaaaa!!!!");
+    }
+else
+{
+    Console.WriteLine("This is fine :)");
+}
